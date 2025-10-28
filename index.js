321c5696253cc6086f7c707c1bac9cbcdaae3345
Nancy Bankson <nbankson3@gmail.com>
2025-10-28 18:20:21 -0500

    Added filter function and html basic elements

--- index.js
+++ index.js
@@ -22,21 +22,35 @@ function displayList(fnList) {
     console.log(fnList)
 }
 
-function filteritems(fnList, searchItem) {
-    
+ function filteritems(fnList, searchItem) {
+    // let lowerCaseITem = toLowerCase(searchItem)
+    // let lowerCaseList = fnList.map(lowerCaseList >= {
+    //     return element.toLowerCase()
+    // })
+    // let filterList = []
+    // for (let i = 0; i < fnList.length; i++) {
+    //     if (fnList[i] <= searchItem) {
+    //         filterList.push(fnList[i])
+    //     }
+    //     return filterList
+    // }
+    let filterList = fnList.filter(currentItem => currentItem.includes(searchItem));
+    return filterList
+ }
+ shoppingList = ["apple", "banana", "appleton", "kiwi", "applebottom"]
+ let mysearch = "apple"
+ console.log(filteritems(shoppingList, mysearch))
 
-}
 
-
-let newItem = prompt("Enter a new item: ")
-addItem(shoppingList, newItem)
-displayList(shoppingList)
-newItem = prompt("Enter another item: ")
-addItem(shoppingList, newItem)
-displayList(shoppingList)
-newItem = prompt("Enter another item: ")
-addItem(shoppingList, newItem)
-displayList(shoppingList)
-console.log("Removing item")
-removeLastItem(shoppingList)
-displayList(shoppingList)
+// let newItem = prompt("Enter a new item: ")
+// addItem(shoppingList, newItem)
+// displayList(shoppingList)
+// newItem = prompt("Enter another item: ")
+// addItem(shoppingList, newItem)
+// displayList(shoppingList)
+// newItem = prompt("Enter another item: ")
+// addItem(shoppingList, newItem)
+// displayList(shoppingList)
+// console.log("Removing item")
+// removeLastItem(shoppingList)
+// displayList(shoppingList)
