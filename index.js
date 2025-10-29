bccbe18a4ad1bb25a1cc40eb9568d38e63f7185a
Nancy Bankson <nbankson3@gmail.com>
2025-10-28 21:51:08 -0500

    Remove button is workinggit add .git add .git add .git add .git add .

--- index.js
+++ index.js
@@ -1,53 +1,65 @@
-const prompt = require('prompt-sync')()
-let shoppingList = []
-let newItem = document.getElementById("input-el")
-let listButton = document.getElementById("button-el")
-let returnList = document.getElementById("list-el")
+// const prompt = require('prompt-sync')()
+let newItem = document.getElementById("input-el");
+let listButton = document.getElementById("button-el");
+let returnList = document.getElementById("list-el");
+let removeButton = document.getElementById("remove-el")
 
-function addItem(fnList, fnItem) {
-    matchingItem = false
-    for (let i = 0; i < fnList.length; i++) {
-        if (fnList[i] === fnItem) {
-            matchingItem = true
-        }}
-    if (matchingItem === true) {
-        return fnList
+// function addItem(fnList, fnItem) {
+//     matchingItem = false
+//     for (let i = 0; i < fnList.length; i++) {
+//         if (fnList[i] === fnItem) {
+//             matchingItem = true
+//         }
+//     }
+//     if (matchingItem === true) {
+//         return fnList
+//     }
+//     else {
+//         let newList = fnList.push(fnItem)
+//         return newList
+//     }
+// }
+// function removeLastItem(fnList) {
+//     let newList = fnList.pop()
+//     return newList
+// }
+// function displayList(fnList) {
+//     console.log(fnList)
+// }
+
+// function filteritems(fnList, searchItem) {
+//     // let lowerCaseITem = toLowerCase(searchItem)
+//     // let lowerCaseList = fnList.map(lowerCaseList >= {
+//     //     return element.toLowerCase()
+//     // })
+//     // let filterList = []
+//     // for (let i = 0; i < fnList.length; i++) {
+//     //     if (fnList[i] <= searchItem) {
+//     //         filterList.push(fnList[i])
+//     //     }
+//     //     return filterList
+//     // }
+//     let filterList = fnList.filter(currentItem => currentItem.includes(searchItem));
+//     return filterList
+// }
+listButton.addEventListener("click", function () {
+    let taskText = newItem.value;
+
+    if (taskText === "") {
+        alert("Please enter a task!");
+        return;
     }
-    else {
-    let newList = fnList.push(fnItem)
-    return newList
-    }}
-function removeLastItem(fnList) {
-    let newList = fnList.pop()
-    return newList
-}
-function displayList(fnList) {
-    console.log(fnList)
-}
 
- function filteritems(fnList, searchItem) {
-    // let lowerCaseITem = toLowerCase(searchItem)
-    // let lowerCaseList = fnList.map(lowerCaseList >= {
-    //     return element.toLowerCase()
-    // })
-    // let filterList = []
-    // for (let i = 0; i < fnList.length; i++) {
-    //     if (fnList[i] <= searchItem) {
-    //         filterList.push(fnList[i])
-    //     }
-    //     return filterList
-    // }
-    let filterList = fnList.filter(currentItem => currentItem.includes(searchItem));
-    return filterList
- }
- listButton.addEventListener("click", function() {
-    taskText = newItem.value
-    let listItem = document.createElement("li")
-    listItem.innerText = taskText
-    shoppingList.appendChild(listItem)
-    taskInput.value = ""
+    let listItem = document.createElement("li");
+    listItem.innerText = taskText;
+
+    returnList.appendChild(listItem);
+    newItem.value = ""; // Clear the input field
+});
+removeButton.addEventListener("click", function() {
+    returnList.removeChild(returnList.lastChild)
+    console.log(returnList)
 })
-
 
 
 //  shoppingList = ["apple", "banana", "appleton", "kiwi", "applebottom"]
